12d05023307c784e10148271cf5dc16a8aabae61
Igor Orszanski <igor.orszanski@gmail.com>
2017-06-19 21:47:18 +0200

    prepare for heroku

--- server.js
+++ server.js
@@ -1,8 +1,17 @@
 const express = require('express');
 
 const app = express();
+const PORT = process.env.PORT || 3000;
+
+// app.use((req, res, next){
+//   if(req.headers['x-forwarded-proto'] === 'http'){
+//     next();
+//   }else{
+//     res.redirect('http://' + req.hostname + req.url);
+//   }
+// })
 app.use(express.static('dist'));
 
-app.listen(3000, () => {
-    console.log('sever up');
+app.listen(PORT, () => {
+    console.log('server up');
 });
