58351e16ede281be4fd499681309c5cf7229e407
Igor Orszanski <igor.orszanski@gmail.com>
2017-06-19 21:59:59 +0200

    mixed content fix

--- server.js
+++ server.js
@@ -3,15 +3,15 @@ const express = require('express');
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// app.use((req, res, next){
-//   if(req.headers['x-forwarded-proto'] === 'http'){
-//     next();
-//   }else{
-//     res.redirect('http://' + req.hostname + req.url);
-//   }
-// })
+app.use((req, res, next) => {
+  if (req.headers['x-forwarded-proto'] === 'http') {
+    next();
+  } else {
+    res.redirect('http://' + req.hostname + req.url);
+  }
+});
 app.use(express.static('dist'));
 
 app.listen(PORT, () => {
-    console.log('server up');
+  console.log('server up');
 });
