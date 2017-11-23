4a316743b5e712cddc80194316a898e878974885
Igor Orszanski <igor.orszanski@gmail.com>
2017-11-23 21:51:26 +0100

    test

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -6,7 +6,7 @@ export default () => {
       <h1 className="text-center">About</h1>
       <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Minus accusantium asperiores error nostrum consectetur explicabo eos placeat blanditiis porro quae sapiente in, tempore, nulla, exercitationem odit nemo enim ipsam possimus!
       </p>
-      <p>Sint explicabo porro, molestiae? Iusto perspiciatis alias voluptatem voluptatibus, architecto magnam neque non voluptate laborum quod vero. Aliquam adipisci error harum nesciunt at maxime repudiandae, asperiores quam accusantium iusto unde!
+      <p>Sint aaaa explicabo porro, molestiae? Iusto perspiciatis alias voluptatem voluptatibus, architecto magnam neque non voluptate laborum quod vero. Aliquam adipisci error harum nesciunt at maxime repudiandae, asperiores quam accusantium iusto unde!
       </p>
     </div>
   );
